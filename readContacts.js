1aac863c30a7a63e745656749292126843155b0b
John Williams <digitaltangos@gmail.com>
2023-02-11 11:33:47 +0000

    Exercise Solution

--- readContacts.js
+++ readContacts.js
@@ -1,28 +1,31 @@
 // generate json with 10 contacts data with fields id, name, email, phone, address, company, jobTitle, notes
 // and save it to contacts.json file
-var faker = require('faker');
 var fs = require('fs');
+// add faker module to generate fake data
+const { faker } = require('@faker-js/faker');
+
 
 function readContacts() {
     var contacts = [];
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 3; i++) {
         contacts.push({
             id: i,
-            name: faker.name.findName(),
+            name: faker.name.fullName(),
             email: faker.internet.email(),
-            phone: faker.phone.phoneNumber(),
+            phone: faker.phone.number(),
             address: faker.address.streetAddress(),
-            company: faker.company.companyName(),
+            company: faker.company.name(),
             jobTitle: faker.name.jobTitle(),
-            notes: faker.lorem.sentence()
+            notes: faker.lorem.notes()
         });
     }
     return contacts;
 }
 var contacts = readContacts();
+contacts = console.log(JSON.stringify(contacts));
 fs.writeFile('contacts.json', JSON.stringify(contacts), function (err) {
     if (err) {
-        console.log(err);
+        console.log("Error" + err);
     }
     console.log('contacts.json file generated');
 }
