b3345b57eabf6cc2e94eb42bcac53b3a2d817c8e
John Williams <digitaltangos@gmail.com>
2023-02-11 12:52:26 +0000

    generate Customers added

---
+++ generateTables.js
@@ -0,0 +1,57 @@
+// generate a table of customers and their orders 
+// and save it to customers.json file 
+// fields: id name, email, orders (id, date, total, status, items (id, name, price, quantity, total))
+const { faker } = require('@faker-js/faker');
+var fs = require('fs');
+
+function getCustomers() {
+    var customers = [];
+    for (var i = 0; i < 3; i++) {
+        customers.push({
+            id: i,
+            name: faker.name.fullName(),
+            email: faker.internet.email(),
+            orders: getOrders()
+        });
+    }
+    return customers;
+}
+function getOrders() {
+    var orders = [];
+    for (var i = 0; i < 3; i++) {
+        orders.push({
+            id: i,
+            date: faker.date.past(),
+            total: faker.commerce.price(),
+            //status: faker.random.arrayElement(['pending', 'delivered', 'cancelled']),
+            items: getItems()
+        });
+    }
+    return orders;
+}
+function getItems() {
+    var items = [];
+    for (var i = 0; i < 3; i++) {
+        items.push({
+            id: i,
+            name: faker.commerce.productName(),
+            price: faker.commerce.price(),
+            // get a random number between 1 and 10
+            quantity: 10,
+            total: faker.commerce.price()
+        });
+    }
+    return items;
+}
+var customers = getCustomers();
+customers = console.log(JSON.stringify(customers));
+// write a json file with the generated data - customers.json take car
+
+fs.writeFile('customers.json', JSON.stringify(customers), function (err) {
+    if (err) {
+        console.log("Error" + err);
+    }
+    console.log('contacts.json file generated');
+}
+);
+
